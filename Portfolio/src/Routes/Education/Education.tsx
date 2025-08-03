556b34bd60759b38583957df697896fad4eb40cc
Tommy <tsegovia20@mail.wou.edu>
2025-08-03 13:28:25 -0700

    Added Education Card on Education Page

--- Portfolio/src/Routes/Education/Education.tsx
+++ Portfolio/src/Routes/Education/Education.tsx
@@ -1,12 +1,67 @@
-
+import Style from './Education.module.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import {BsMortarboardFill} from 'react-icons/bs';
 function Education(){
     return(
-        <div id="mainPage">
-            <h1>My Education</h1>
-            <div id="summaryDiv">
-                <p>
-                    Education
-                </p>
+        <div className={Style.mainPage}>
+            <div className={Style.mainCard}>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className={Style.title}>Education </h1>
+                            <h3 className={Style.subInfo}>
+                                <BsMortarboardFill /> Bachelor of Science in Computer Science <br></br>
+                                <span style={{color: 'orange'}}>3.65 GPA - Honor Student: Magna Cum Laude</span>
+                            </h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className={Style.classList}>
+                            <h2 className={Style.subtitle}>
+                                Courses
+                            </h2>
+                            <h3>
+                                Software Engineering <br></br>
+                                Cybersecurity & Professional Practices <br></br>
+                                Cross Platform Application Development <br></br>
+                                Software Design & Testing <br></br>
+                                Database Management <br></br>
+                                Operating Systems <br></br>
+                                Computer Architecture <br></br>
+                                Computer Networks <br></br>
+                                Computer Organization <br></br>
+                                Information Management <br></br>
+                                Languages and Tools <br></br>
+                                Programming Languages <br></br>
+                                Computer Graphics <br></br>
+                                Data Structures<br></br>
+                                Algorithms <br></br>
+                            </h3>
+                        </Col>
+                        <Col className={Style.focusList}>
+                            <h2 className={Style.subtitle}>
+                                Academic Focuses
+                            </h2>
+                            <h3>
+                                RESTful API Development <br></br>
+                                CI/CD Practices <br></br>
+                                Software Engineering Principles<br></br>
+                                Full-Stack Development <br></br>
+                                Database Design <br></br>
+                                Web Development <br></br>
+                                Artificial Intelligence <br></br>
+                                Automation Testing  <br></br>
+                                Cybersecurity <br></br>
+                                Operating Systems <br></br>
+                                Computer Architecture <br></br>
+                                Computer Networks <br></br>
+                                Version Control and Collaboration <br></br>
+                                Agile Methodologies <br></br>
+                                Data Structures and Algorithms <br></br>
+                            </h3>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </div>
     )
